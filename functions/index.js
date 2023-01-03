f361fee7b857a0d694902396c908a620b4b63565
Aramide Ramadan <92337603+mister-abdurahman@users.noreply.github.com>
2023-01-03 12:56:41 +0100

    .

--- functions/index.js
+++ functions/index.js
@@ -18,14 +18,12 @@ import User from "../models/User.js";
 import Post from "../models/Post.js";
 import { users, posts } from "../data/index.js";
 
-/* CONFIGURATIONS */
-// const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(
-  "C:/Users/aramr/Documents/GitHub/backend-social-app/functions/index.js"
-);
-// const __dirname = path.dirname(__filename);
-// console.log(__dirname, __filename);
 dotenv.config();
+console.log(process.env.MONGO_URL);
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(helmet());
