e0929abc989d3550d5e940df549455650b650fc9
Aramide Ramadan <92337603+mister-abdurahman@users.noreply.github.com>
2023-01-03 12:44:29 +0100

    .

--- functions/index.js
+++ functions/index.js
@@ -19,7 +19,7 @@ import Post from "../models/Post.js";
 import { users, posts } from "../data/index.js";
 
 /* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(
   "C:/Users/aramr/Documents/GitHub/backend-social-app/functions/index.js"
 );
