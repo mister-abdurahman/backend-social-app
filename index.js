4e4c31d02d677ab20b17595a7333b0d14441420e
Aramide Ramadan <92337603+mister-abdurahman@users.noreply.github.com>
2023-01-04 12:19:22 +0100

    .

---
+++ index.js
@@ -0,0 +1,118 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import fetchNoCors from "fetch-no-cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import * as url from "url";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
+dotenv.config();
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __theDirname = path.dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// const corsOptions = {
+//   origin: "https://relaxed-scone-3e19fa.netlify.app/",
+//   optionsSuccessStatus: 200,
+// };
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Accept, X-Requested-With, Authorization, Content-Type, x-custom-header"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,PATCH,DELETE,OPTIONS"
+  );
+  next();
+});
+
+app.use("/assets", express.static(path.join(__theDirname, "public/assets")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome.!");
+});
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+// Setting dirname in module
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+// Serving the front End
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+//Set static folder
+// app.use(express.static("client/build"));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+// app.get("*", (_, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
